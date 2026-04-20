f2620a90591e4dac117294c91b6670dbb9c74966
prasadk <prasadkul2009@gmail.com>
2026-04-20 21:17:59 +0530

    let var & const test

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console("this is my first java program");  
+console.log("this is my first java program");  
+let city = "Delhi";
+console.log(city);
+const weight = 70;
+console.log(weight);
+var name= "Jhon";
+console.log(name);
