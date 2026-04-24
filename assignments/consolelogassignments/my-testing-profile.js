64751571bf5c146b6ccbedf2703333787fe62c0f
prasadk <prasadkul2009@gmail.com>
2026-04-24 14:59:21 +0530

    my testing profile assignments

---
+++ assignments/consolelogassignments/my-testing-profile.js
@@ -0,0 +1,10 @@
+// Print your name
+console.log("Prasad Kulkarni");
+// Print your role (e.g., "Manual Tester")
+console.log("Manual Tester");
+// Print years of testing experience
+console.log("Years of testing experience 13 yrs");
+// Print one testing tool you use
+console.log("Testing tool I used is Playwright");
+// Print why you're learning JavaScript
+console.log("learning Javascripts is current market and industry demand and usefull for web automations");
