7d05f0fb49d41cdab00763e275930f5d5a2dfda9
prasadk <prasadkul2009@gmail.com>
2026-04-26 23:04:40 +0530

    added comparison logical operator

---
+++ assignments/7-operators-comparison-exercise/comparison-logical-operators.js
@@ -0,0 +1,42 @@
+console.log("=======Comparison-Logical-Operators===============================");  
+let age = 25;
+if (age >= 18 && age <= 65){
+    console.log("✅ Valid age range (18-65)")
+} 
+else {
+    console.log("❌ Age must be between 18 and 65");
+}
+console.log("---------------------------------------------------------------");
+let age1 = 18;
+if (age1 >= 18 && age <= 65){
+    console.log("✅ Valid age range (18-65)")
+} 
+else {
+    console.log("❌ Age must be between 18 and 65");
+}
+console.log("---------------------------------------------------------------");
+let age2 = 17;
+if (age2 >= 18 && age2 <= 65){
+    console.log("✅ Valid age range (18-65)")
+} 
+else {
+    console.log("❌ Age must be between 18 and 65");
+}
+console.log("---------------------------------------------------------------");
+let age4 = 66;
+if (age4 >= 18 && age4 <= 65)
+{
+    console.log("✅ Valid age range (18-65)")
+} 
+else {
+    console.log("❌ Age must be between 18 and 65");
+}
+console.log("---------------------------------------------------------------");
+let age5 = 25;
+if (age5 >= 18 && age5 <= 65){
+    console.log("✅ Valid age range (18-65)")
+} 
+else {
+    console.log("❌ Age must be between 18 and 65");
+}
+console.log("---------------------------------------------------------------");
