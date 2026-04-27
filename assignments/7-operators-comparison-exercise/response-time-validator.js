ee2563295648868aeccab1fa949d8d03a14198e0
prasadk <prasadkul2009@gmail.com>
2026-04-27 19:23:39 +0530

    added test status validator

---
+++ assignments/7-operators-comparison-exercise/response-time-validator.js
@@ -0,0 +1,19 @@
+console.log("                                                       ");
+console.log("====================================================== ");
+console.log("              TEST RESPONSE VALIDATOR                  ");
+console.log("====================================================== ");
+console.log("                                                       ");
+let responseTime = 2.5;
+function showResults(responseTime , status) {
+
+if (responseTime < 3) {status== "PASSED" (responsetime ==2.5 ) 
+    {
+    console.log("✅ Test Passed");
+    }
+    }   
+    
+    else {status== "FAILED" (responsetime ==2.5 )
+    console.log("❌ Test Failed");
+    }  
+}
+showResults(2.5, "PASSED");
--- assignments/7-operators-comparison-exercise/test-status-validator.js
+++ assignments/7-operators-comparison-exercise/test-status-validator.js
@@ -3,18 +3,16 @@ console.log("====================================================== ");
 console.log("              TEST STATUS VALIDATOR                    ");
 console.log("====================================================== ");
 console.log("                                                       ");
-function showTestResults (status, errorcount) {
-    if (status="Passed" , errorCount= 0) 
+let errorCount = 0;
+function showTestResults(status, errorcount) {
+    if (status=="Passed" && errorCount== 0) 
         {
         console.log("✅ Test Passed");
         }
-    else console.log("❌Test Failed");    
- 
+    else { console.log("❌Test Failed");    
+    }
 }
-/*let status = "PASSED";
-console.log("Test Status: " + status);
-let errorCount ="0";
-console.log("Error Count: " + errorCount);
-if ((status == "PASSED" && errorCount == "0"))
-    console.log("✅ Test completed successfully");
-else */
+showTestResults("Passed", 0);
+showTestResults("Failed", 0);
+showTestResults("Passed", 1);
+showTestResults("Failed", 3);
