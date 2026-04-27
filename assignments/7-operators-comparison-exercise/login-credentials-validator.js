470091273486fc81d1cff869e4d2e5860053ae4e
prasadk <prasadkul2009@gmail.com>
2026-04-27 18:36:56 +0530

    added missing code in login-credentials-validator

--- assignments/7-operators-comparison-exercise/login-credentials-validator.js
+++ assignments/7-operators-comparison-exercise/login-credentials-validator.js
@@ -10,14 +10,24 @@ else {
 
 //Test Case `username="testuser"`, `password=""`, `email=""`
 
-/*let username= "testuser";
-let password= "";
-let email= "";
-if ((username !== "" && password !=="") || email !=="") {
+let username2= "testuser";
+let password2= "";
+let email2= "";
+if ((username2 !== "" && password2 !=="") || email2 !=="") {
     console.log("✅ Sufficient login credentials");
 }
 else {
     console.log("❌ Insufficient credentials - provide username+password OR email ");
-}*/
+}
 
+//Test Case `username=""`, `password=""`, `email="test@example.com"`
+let username3= "testuser";
+let password3= "";
+let email3= "";
+if ((username3 !== "" && password3 !=="") || email3 !=="") {
+    console.log("✅ Sufficient login credentials");
+}
+else {
+    console.log("❌ Insufficient credentials - provide username+password OR email ");
+}
 
