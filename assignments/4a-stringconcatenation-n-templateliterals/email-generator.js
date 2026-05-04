aea045c2c5477bb8d1dae6956e20cee8a9b00671
prasadk <prasadkul2009@gmail.com>
2026-05-04 14:56:52 +0530

    added email generated code

---
+++ assignments/4a-stringconcatenation-n-templateliterals/email-generator.js
@@ -0,0 +1,19 @@
+// User 1:
+let firstname ="jhon";
+let lastname ="Doe";
+let emailcontac = firstname + "." + lastname + "@example.com";  
+console.log("Email (Concatenate)" + emailcontac);
+
+console.log("===============================================");
+//User2: 
+let firstname1 ="ram";
+let lastname1 = "sharma";
+let emailcontc1 = firstname1 + "."+lastname1 +"@example.com";
+console.log("Email (Concatenate)" + emailcontc1);
+console.log("===============================================");
+
+let firstname2 ="ganesh";
+let lastanme2 = "rajaram";
+let emailcontc2 = firstname2 + "." + lastanme2 + "@example.com";
+console.log("Email (Concatenate)" + emailcontc2);
+console.log("===============================================");
