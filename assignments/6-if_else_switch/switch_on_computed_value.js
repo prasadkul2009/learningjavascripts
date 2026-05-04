80319d10638fd411a6364707395940f8d521cd6e
prasadk <prasadkul2009@gmail.com>
2026-05-04 12:07:52 +0530

    added switch_on_computed_value code

---
+++ assignments/6-if_else_switch/switch_on_computed_value.js
@@ -0,0 +1,20 @@
+console.log("8a: Switch on a computed value (grade buckets)");
+
+let score =50;
+switch(Math.floor (score/10)){
+    case 10:
+    case 9:
+    console.log("Grade: A (Excellent)");
+    break;
+    case 8:
+    console.log("Grade: B (Good)");
+    break;
+    case 7:
+    console.log("Grade: C (Average)");
+    break;
+    case 6:
+    console.log("Grade: D (Pass)");
+    break;
+    default:
+    console.log("Grade: F (Fail)");
+}
