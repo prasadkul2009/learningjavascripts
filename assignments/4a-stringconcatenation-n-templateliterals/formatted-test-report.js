d8d299b4f900a38c7f958f20fcd12d59f110b7e1
prasadk <prasadkul2009@gmail.com>
2026-05-04 15:11:05 +0530

    added formatted test report

---
+++ assignments/4a-stringconcatenation-n-templateliterals/formatted-test-report.js
@@ -0,0 +1,7 @@
+console.log("Part 2: Format Test Report");
+
+let testName= "Login Functionality";
+let tester= "Priya";
+let duration= 2.5;
+let status ="Passed";
+console.log (`Test: ${testName} & Tester: ${tester} & Duration: ${duration} & Status: ${status}`);
