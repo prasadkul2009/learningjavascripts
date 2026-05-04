71d156f4f8bc5def2fffacad75c0b487115a24cf
prasadk <prasadkul2009@gmail.com>
2026-05-04 17:30:02 +0530

    added test_score_analysis code

---
+++ assignments/8-arrays-basics-exercise/test_score_analysis.js
@@ -0,0 +1,4 @@
+let testScores = [85, 92, 78, 95, 88, 73, 90];
+console.log("Total Number of Score: " + testScores.length);
+console.log("First Score: " + testScores[0]);
+console.log("Last Score: " + testScores[6]);
