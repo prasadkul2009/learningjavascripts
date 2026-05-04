796d4ca3c9c51e56e848c33baca8398d58102e2a
prasadk <prasadkul2009@gmail.com>
2026-05-04 16:20:59 +0530

    added arrys test tracking code

---
+++ assignments/8-arrays-basics-exercise/arrays-test-tracking.js
@@ -0,0 +1,7 @@
+console.log("Exercise 1: Test Suite Array");
+console.log("============================================");
+let testCases = ["Valid credentials" , "Invalid password" , "Empty username" , "Empty password" , "Remember me checkbox"];
+console.log(testCases);
+console.log("Total Test Cases: "+ testCases.length);
+console.log("First Test Case: " + testCases [0]);
+console.log("Last Test Cases: " + testCases [testCases.length-1]);
